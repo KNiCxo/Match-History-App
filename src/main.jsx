d71fc47e8f6abe364742a273a8a92d94809fde41
KNiC.xo <ntcaliwag@hotmail.com>
2024-06-20 00:52:19 -0700

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import HomePage from './home-page/home-page.jsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    // Main page
+    path: '/',
+    element: <HomePage></HomePage>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}></RouterProvider>
+)
