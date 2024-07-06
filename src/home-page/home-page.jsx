fca088fe3329496b8cacd8b8766067b41d5eab98
KNiC.xo <ntcaliwag@hotmail.com>
2024-07-06 07:07:47 -0700

    Added documentation to home page

--- src/home-page/home-page.jsx
+++ src/home-page/home-page.jsx
@@ -1,8 +1,19 @@
 import './home-page.css'
 
+// App home page where users can enter their "Riot ID" and get their match history
 function HomePage() {
+  // When called, displays the list of regions to change to
+  function showRegionMenu() {
+    document.querySelector('.region-options').style.display = 'block';
+  }
+
+  // When called, sets the region based on what the user picked
+  function changeRegion(region) {
+    console.log(region);
+  }
+
+  // If body is clicked and the region elements weren't the target, hide the region options
   document.body.addEventListener('click', (e) => {
-    console.log(e.target.className);
     if (e.target.className != 'region-name' &&
         e.target.className != 'region-label' &&
         e.target.className != 'region-text') {
@@ -10,13 +21,10 @@ function HomePage() {
     }
   });
 
-  function showRegionMenu() {
-    document.querySelector('.region-options').style.display = 'block';
-  }
-
   return(
     <>
       <div className='container'>
+        {/* Contains the website logo and headers */}
         <div className='header-div'>
           <img src="logo.webp" alt="" />
 
@@ -27,17 +35,22 @@ function HomePage() {
           <h2>View Detailed League of Legends Match History Stats</h2>
         </div>
 
+        {/* Contains the elements for the search bar */}
         <div className='search-bar'>
+          {/* Contains region selection and user input elements */}
           <div className='top-search'>
             <div className='select-region' onClick={showRegionMenu}>
+              {/* Current region selected */}
               <div className='region-text'>
                 <span className='region-label'>Region</span>
                 <span className='region-name'>NA</span>
               </div>
 
+              {/* List of regions to change to */}
+              {/* Clicking on an option changes the current region to that region */}
               <div className='region-options'>
                 <ul>
-                  <li>NA</li>
+                  <li onClick={() => changeRegion('NA')}>NA</li>
                   <li>EUW</li>
                   <li>EUNE</li>
                   <li>OCE</li>
@@ -55,9 +68,11 @@ function HomePage() {
               </div>
             </div>
 
+            {/* User input */}
             <input className='user-input' type="text" placeholder="Enter RIOT ID"/>
           </div>
-
+          
+          {/* Search button */}
           <button className='search-button'>Search</button>
         </div>
       </div>
