500519058c5bdf588c8626ca428516627bd389d4
KNiC.xo <ntcaliwag@hotmail.com>
2024-07-06 06:02:05 -0700

    Added UI to search page

--- src/home-page/home-page.jsx
+++ src/home-page/home-page.jsx
@@ -1,39 +1,67 @@
 import './home-page.css'
 
 function HomePage() {
+  document.body.addEventListener('click', (e) => {
+    console.log(e.target.className);
+    if (e.target.className != 'region-name' &&
+        e.target.className != 'region-label' &&
+        e.target.className != 'region-text') {
+      document.querySelector('.region-options').style.display = 'none';
+    }
+  });
+
+  function showRegionMenu() {
+    document.querySelector('.region-options').style.display = 'block';
+  }
+
   return(
     <>
-      <div className='search-bar'>
-        <img src="logo.webp" alt="" />
-        <h1>
-          League of Legends Stats
-        </h1>
+      <div className='container'>
+        <div className='header-div'>
+          <img src="logo.webp" alt="" />
 
-        <label htmlFor="">Region</label>
-        <select name="region" id="">
-          <option value="na1">NA</option>
-          <option value="euw1">EUW</option>
-          <option value="eun1">EUNE</option>
-          <option value="oce1">OCE</option>
-          <option value="kr">KR</option>
-          <option value="jp1">JP</option>
-          <option value="br1">BR</option>
-          <option value="la2">LAS</option>
-          <option value="la1">LAN</option>
-          <option value="ru">RU</option>
-          <option value="tr1">TR</option>
-          <option value="sg2">SG</option>
-          <option value="ph2">PH</option>
-          <option value="tw2">TW</option>
-          <option value="vn2">VN</option>
-          <option value="th2">TH</option>
-        </select>
+          <h1>
+            League of Legends Stats
+          </h1>
 
-        <label htmlFor="">Search</label>
-        <input type="text" placeholder="Enter RIOT ID"/>
+          <h2>View Detailed League of Legends Match History Stats</h2>
+        </div>
+
+        <div className='search-bar'>
+          <div className='top-search'>
+            <div className='select-region' onClick={showRegionMenu}>
+              <div className='region-text'>
+                <span className='region-label'>Region</span>
+                <span className='region-name'>NA</span>
+              </div>
+
+              <div className='region-options'>
+                <ul>
+                  <li>NA</li>
+                  <li>EUW</li>
+                  <li>EUNE</li>
+                  <li>OCE</li>
+                  <li>KR</li>
+                  <li>BR</li>
+                  <li>LAS</li>
+                  <li>LAN</li>
+                  <li>RU</li>
+                  <li>SG</li>
+                  <li>PH</li>
+                  <li>TW</li>
+                  <li>VN</li>
+                  <li>TH</li>
+                </ul>
+              </div>
+            </div>
+
+            <input className='user-input' type="text" placeholder="Enter RIOT ID"/>
+          </div>
+
+          <button className='search-button'>Search</button>
+        </div>
       </div>
     </>
-
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <HomePage></HomePage>
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}></RouterProvider>
