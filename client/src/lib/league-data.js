d0b82e115bf28aa80b372f07023cd1c4106b82b6
KNiC.xo <ntcaliwag@hotmail.com>
2024-07-25 13:57:18 -0700

    Added library folder and league-data.js file

---
+++ client/src/lib/league-data.js
@@ -0,0 +1,23 @@
+const runes = {
+  8100: "Domination",
+  8112: "Electrocute",
+  8128: "Dark Harvest",
+  9923: "Hail of Blades",
+  8300: "Inspiration",
+  8351: "Glacial Augment",
+  8360: "Unsealed Spellbook",
+  8369: "First Strike",
+  8000: "Precision",
+  8005: "Press the Attack",
+  8021: "Fleet Footwork",
+  8010: "Conqueror",
+  8400: "Resolve",
+  8437: "Grasp of the Undying",
+  8439: "Aftershock",
+  8465: "Demolish",
+  8200: "Sorcery",
+  8214: "Summon Aery",
+  8229: "Arcane Comet",
+  8230: "Phase Rush"
+}
+
